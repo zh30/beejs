847825f5745d375392d6253f944d8927d38e9778
Henry Zhang <hello@zhanghe.dev>
2025-12-17 22:36:27 +0800

    cleanup: 清理测试文件

--- debug_main.js
+++
@@ -1,5 +0,0 @@
-// Main file
-const mod = require('./debug_module.js');
-console.log('Module loaded:', mod);
-console.log('Value:', mod.value);
-console.log('Add:', mod.add(5, 3));
--- debug_module.js
+++
@@ -1,5 +0,0 @@
-// Test module
-module.exports = {
-    value: 42,
-    add: (a, b) => a + b
-};
